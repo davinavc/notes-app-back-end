fef15be45f8eb7c8ebedd203cc8c7f149370eaf8
Davina <davinavc18@gmail.com>
2023-07-02 22:49:13 +0700

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const Hapi = require('@hapi/hapi');
 // const routes = require('./route');
 
-const init = aync () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
-    });
-    await server.start();
-    console.log(`server berjalan ${server.info.uri}`);
-}
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+  await server.start();
+  console.log(`server berjalan pada ${server.info.uri}`);
+};
 
 init();
