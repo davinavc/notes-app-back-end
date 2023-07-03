bd8c2deebf78bd6bb05808a42b9b6c43e98accec
Davina <davinavc18@gmail.com>
2023-07-03 20:20:07 +0700

    Make function to save notes, update notes

--- src/handler.js
+++ src/handler.js
@@ -41,4 +41,77 @@ const addNoteHandler = (req, h) => {
   return response;
 };
 
-module.exports = { addNoteHandler };
+// buat ngembaliin data dengan nilai notes di dalamnya
+const getAllNotesHandler = () => ({
+    status: 'success',
+    data: {
+      notes,
+    },
+  });
+
+const getNoteByIdHandler = (req, h) => {
+  // buat dapetin id dari request params
+  const { id } = req.params;
+  // buat dapetin objek note dengan id dari objek array note
+  // pake filter() buat dapetin objek nya
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined){
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+const editNoteByIdHandler = (req, h) => {
+  // buat dapetin nilai id karna diedit berdasarkan id
+  const { id } = req.params;
+  const { title, tags, body } = req.payload;
+  const updatedAt = new Date().toISOString();
+  // findindex buat dapetin dulu index array pada objek sesuai id
+  const index = notes.findIndex((note) => note.id === id);
+
+  //  kalo misalkan note dengan id yg dicari ada maka index akan bernilai array index dari objek yg
+  // dicari. Tapi kalo tidak ditemukan idnya maka index bernilai -1
+  // if ini respons ketika index ditemukan idnya
+  if (index !== -1){
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+    const response = h.response({
+      status: 'sucess',
+      message: 'Catatan berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+
+  // respon ketika id gagal ditemukan
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui catan. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+};
--- src/route.js
+++ src/route.js
@@ -1,10 +1,31 @@
-const { addNoteHandler } = require('./handler');
+const {
+  addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler,
+} = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+  },
+  {
+    // baut nampilin catatan nya di homepage
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    // buat nampilin catatannya berdasarkan id
+    // misal simpen catatan travel nah nampilin page travel itu doang
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    // baut nampilin catatan nya di homepage
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
   },
 ];
 module.exports = routes;
